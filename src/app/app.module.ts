edc07a15fa30b19bda5d011818fbd106fa6cc8ab
Valeria <79008016+leraleraLL@users.noreply.github.com>
2022-05-19 14:01:41 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,7 +50,14 @@ export function HttpLoaderFactory(http: HttpClient) {
     MatInputModule,
     ReactiveFormsModule,
     MatIconModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyDl76gdIM_VNyaCYmijm_KLkCF-AIlKbzA",
+      authDomain: "cryptoc-db46c.firebaseapp.com",
+      projectId: "cryptoc-db46c",
+      storageBucket: "cryptoc-db46c.appspot.com",
+      messagingSenderId: "302637864844",
+      appId: "1:302637864844:web:7d9ebfafa6880c385de8a9"
+    })),
     provideAuth(() => getAuth()),
     HotToastModule.forRoot(),
     MatMenuModule,
