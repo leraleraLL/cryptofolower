d3a4a7437595e0f7df16cf96390474cb46cf6c42
Valeria <79008016+leraleraLL@users.noreply.github.com>
2022-05-19 14:34:38 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase : {
+    apiKey: "AIzaSyDl76gdIM_VNyaCYmijm_KLkCF-AIlKbzA",
+    authDomain: "cryptoc-db46c.firebaseapp.com",
+    projectId: "cryptoc-db46c",
+    storageBucket: "cryptoc-db46c.appspot.com",
+    messagingSenderId: "302637864844",
+    appId: "1:302637864844:web:7d9ebfafa6880c385de8a9"
+  }
 };
